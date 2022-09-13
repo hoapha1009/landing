9d782493d78ab9c4161d80795a7fc80609bd2a2e
HoangPham <hoangpm@greengroups.vn>
2022-09-13 21:02:46 +0700

    ADD: điều chỉnh hiển thị layout và cài đặt settings.json

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,18 @@
-import * as React from 'react';
+import Header from "@/components/layout/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
-  return <>{children}</>;
+ return (
+  <div
+  className='relative min-h-screen bg-white text-gray-700'
+  style={{
+    fontFamily: 'Noto Sans',
+  }}
+  >
+    <>
+      <Header />
+      <div className=''>{children}</div>
+    </>
+  </div>
+ )
 }
