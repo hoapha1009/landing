71b9f94414d2a6eeef56a5374d1536c4752d01ff
HoangPham <hoangpm@greengroups.vn>
2022-09-13 20:46:19 +0700

    ADD: Chỉnh style nút button primary và outline

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -45,16 +45,16 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           //#region  //*=========== Variants ===========
           [
             variant === 'primary' && [
-              'bg-primary-500 text-white',
-              'border border-primary-600',
-              'hover:bg-primary-600 hover:text-white',
-              'active:bg-primary-500',
-              'disabled:bg-primary-400 disabled:hover:bg-primary-400',
+              'bg-primary text-white',
+              'border border-primary',
+              'hover:text-white hover:brightness-90',
+              'active:bg-primary',
+              'disabled:bg-primary disabled:hover:bg-primary',
             ],
             variant === 'outline' && [
-              'text-primary-500',
-              'border border-primary-500',
-              'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
+              'text-primary',
+              'border border-primary',
+              'hover:bg-primary  hover:text-white active:bg-primary disabled:bg-primary',
               isDarkBg &&
                 'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
             ],
