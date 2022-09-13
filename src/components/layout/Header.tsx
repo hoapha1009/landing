5f4a107835f108d87fb84fd88a494f5b8aaa0d27
HoangPham <hoangpm@greengroups.vn>
2022-09-13 20:51:26 +0700

    ADD: Thêm các file ảnh banner và logo; Code thêm phần header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,31 +1,101 @@
-import * as React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useMemo, useState } from 'react';
+import { RiArrowDownSLine } from 'react-icons/ri';
 
-import UnstyledLink from '@/components/links/UnstyledLink';
-
-const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
-];
+import Button from '../buttons/Button';
+import NextImage from '../NextImage';
 
 export default function Header() {
+  const router = useRouter();
+  const [scrollTop, setScrollTop] = useState(0);
+  const isScrolled = useMemo(() => scrollTop > 50, [scrollTop]);
+  const onScroll = (e: any) => {
+    setScrollTop(e.target.documentElement.scrollTop);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
-    <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
-        </UnstyledLink>
-        <nav>
-          <ul className='flex items-center justify-between space-x-4'>
-            {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
-                  {label}
-                </UnstyledLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
+    <header className='z-100 sticky top-0 w-full'>
+      <div
+        className={`flex h-20 items-center justify-between bg-white px-14 text-lg transition-all
+    ${
+      isScrolled
+        ? 'border-gray border-b shadow-sm'
+        : 'border-transparent shadow-none'
+    }
+    `}
+      >
+        <div className='flex items-center gap-1'>
+          <Link href='/'>
+            <a className='mr-8 block'>
+              <NextImage
+                src='/images/logo.png'
+                alt='logo'
+                width={102}
+                height={36}
+              />
+            </a>
+          </Link>
+          {MENU_TAB_LIST.map((tab, index) =>
+            tab?.subTabs ? (
+              <div key={index} className='group relative inline-block'>
+                <button className='inline-flex items-center rounded hover:bg-gray-100'>
+                  <span className='mr-1 px-4 py-2'>{tab.title}</span>
+                  <i className='text-xl'>
+                    <RiArrowDownSLine />
+                  </i>
+                </button>
+                <ul className='absolute hidden rounded shadow group-hover:block'>
+                  {tab.subTabs.map((subTab, index) => (
+                    <li key={index} className=''>
+                      <Link href={subTab.href}>
+                        <a className='block whitespace-nowrap py-3 px-8 hover:bg-gray-100'>
+                          {subTab.title}
+                        </a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : (
+              <Link key={index} href={tab.href}>
+                <a
+                  className={`block rounded px-4 py-2 hover:bg-gray-100 ${
+                    router.asPath.startsWith(tab.href) && 'text-primary'
+                  }`}
+                >
+                  {tab.title}
+                </a>
+              </Link>
+            )
+          )}
+        </div>
+        <div className=''>
+          <Button variant='primary'>Đăng ký tư vấn</Button>
+        </div>
       </div>
     </header>
   );
 }
+
+const MENU_TAB_LIST = [
+  { href: '/about-us', title: 'Về GAT' },
+  {
+    title: 'Giải pháp',
+    subTabs: [
+      { href: '/green-agri', title: 'Green Agri' },
+      { href: '/green-check', title: 'Green Check' },
+    ],
+  },
+  { href: '/features', title: 'Tính năng' },
+  { href: '/recruit', title: 'Tuyển dụng' },
+  { href: '/guild', title: 'Hướng dẫn' },
+  { href: '/news-and-blog', title: 'Tin tức & Blog' },
+  { href: '/join-us', title: 'Tham gia cộng đồng' },
+];
