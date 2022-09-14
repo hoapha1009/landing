63eea85867b646ff184846b023ccb0b3a402ea0a
HoangPham <hoangpm@greengroups.vn>
2022-09-14 11:00:37 +0700

    ADD: config tailwind + xóa layout trong index page

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,18 @@
-import Header from "@/components/layout/Header";
+import Header from '@/components/layout/Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
- return (
-  <div
-  className='relative min-h-screen bg-white text-gray-700'
-  style={{
-    fontFamily: 'Noto Sans',
-  }}
-  >
-    <>
-      <Header />
-      <div className=''>{children}</div>
-    </>
-  </div>
- )
+  return (
+    <div
+      className='relative min-h-screen bg-white text-gray-700'
+      style={{
+        fontFamily: 'Noto Sans',
+      }}
+    >
+      <>
+        <Header />
+        <div className=''>{children}</div>
+      </>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,307 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { fontFamily } = require('tailwindcss/defaultTheme');
+const colors = async() => await import('tailwindcss/colors')
 
-/** @type {import('tailwindcss').Config} */
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
+function getSemanticColors(color) {
+  return {
+    light: color[100],
+    DEFAULT: color[500],
+    dark: color[600]
+  }
+}
+
+function generateSemanticColors() {
+  const semanticColors = {}
+  for (const color in colors) {
+    if (typeof colors[color] !== "string") {
+      semanticColors[color] = getSemanticColors(colors[color])
+    }
+  }
+  return semanticColors
+}
+
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  mode: 'jit',
+  content: ['./**/*.ts', './**/*.tsx'],
+  safelist: [
+    {
+      pattern: /grid-cols-.+/,
+    },
+    {
+      pattern: /col-span-.+/,
+    },
+    {
+      pattern: /text-.+/,
+    },
+    {
+      pattern: /bg-.+/,
+    },    
+    {
+      pattern: /animate-.+/,
+    },
+    {
+      pattern: /react-select.*/,
+    },
+    {
+      pattern: /react-datepicker.*/,
+    },
+    {
+      pattern: /swiper.*/,
+    },
+  ],
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',      
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
-      fontFamily: {
-        primary: ['Inter', ...fontFamily.sans],
-      },
       colors: {
-          primary: {
-            light: '#b9f8dd',
-            DEFAULT: '#10AE6A',
-            dark: '#253B2F'
-          },
-          secondary: '#4B8FF8'
+        primary: {
+          light: 'var(--color-primary-light)',
+          DEFAULT: 'var(--color-primary)',
+          dark: 'var(--color-primary-dark)',
+        },
+        accent: {
+          light: 'var(--color-accent-light)',
+          DEFAULT: 'var(--color-accent)',
+          dark: 'var(--color-accent-dark)',
+        },
+        info: getSemanticColors(colors.blue),
+        success: getSemanticColors(colors.green),
+        warning: getSemanticColors(colors.yellow),
+        danger: getSemanticColors(colors.red),
+        ...generateSemanticColors(),
+        primaryGreen: {
+          DEFAULT: '#31C931',
+        },
+        primaryBlack: {
+          DEFAULT: '#4D4D4D',
+        },
+        primaryTurquoise: {
+          DEFAULT: '#53D1B6',
+        },
+        primaryRed: {
+          DEFAULT: '#E74C3C',
+        },
+        neutralDark: {
+          DEFAULT: '#223263',
+        },
+        neutralGrey: {
+          DEFAULT: '#9098B1',
+        },
+        neutralLight: {
+          DEFAULT: '#ECF0F1',
+        },
+      },
+      maxWidth: {
+        1: '0.25rem',
+        2: '0.5rem',
+        3: '0.75rem',
+        4: '1rem',
+        5: '1.25rem',
+        6: '1.5rem',
+        7: '1.75rem',
+        8: '2rem',
+        9: '2.25rem',
+        10: '2.5rem',
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
+        15: '3.75rem',
+        16: '4rem',
+        18: '4.5rem',
+        20: '5rem',
+        22: '5.5rem',
+        24: '6rem',
+        28: '7rem',
+        '4xs': '8rem',
+        '3xs': '12rem',
+        '2xs': '16rem',
+      },
+      minWidth: {
+        'none': 'none',
+        1: '0.25rem',
+        2: '0.5rem',
+        3: '0.75rem',
+        4: '1rem',
+        5: '1.25rem',
+        6: '1.5rem',
+        7: '1.75rem',
+        8: '2rem',
+        9: '2.25rem',
+        10: '2.5rem',
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
+        15: '3.75rem',
+        16: '4rem',
+        18: '4.5rem',
+        20: '5rem',
+        22: '5.5rem',
+        24: '6rem',
+        28: '7rem',
+        '4xs': '8rem',
+        '3xs': '12rem',
+        '2xs': '16rem',
+        'xs': '20rem',
+        'sm': '24rem',
+        'md': '28rem',
+        'lg': '32rem',
+        'xl': '36rem',
+        '2xl': '42rem',
+        '3xl': '48rem',
+        '4xl': '56rem',
+        '5xl': '64rem',
+        '6xl': '72rem',
+        '7xl': '80rem',
+        'screen-sm': '640px',
+        'screen-md': '768px',
+        'screen-lg': '1024px',
+        'screen-xl': '1280px',
+        'screen-2xl': '1536px',
+      },
+      height: {
+        18: '4.5rem',
+        22: '5.5rem',
+      },
+      minHeight: {
+        'none': 'none',
+        1: '0.25rem',
+        2: '0.5rem',
+        3: '0.75rem',
+        4: '1rem',
+        5: '1.25rem',
+        6: '1.5rem',
+        7: '1.75rem',
+        8: '2rem',
+        9: '2.25rem',
+        10: '2.5rem',
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
+        15: '3.75rem',
+        16: '4rem',
+        18: '4.5rem',
+        20: '5rem',
+        22: '5.5rem',
+        24: '6rem',
+        28: '7rem',
+        '4xs': '8rem',
+        '3xs': '12rem',
+        '2xs': '16rem',
+        'xs': '20rem',
+        'sm': '24rem',
+        'md': '28rem',
+        'lg': '32rem',
+        'xl': '36rem',
+        '2xl': '42rem',
+        '3xl': '48rem',
+        '4xl': '56rem',
+        '5xl': '64rem',
+        '6xl': '72rem',
+        '7xl': '80rem',
+        'min': 'min-content',
+      },
+      borderRadius: {
+        inherit: 'inherit'
       },
       keyframes: {
-        flicker: {
-          '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
-            opacity: 0.99,
-            filter:
-              'drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))',
-          },
-          '20%, 21.999%, 63%, 63.999%, 65%, 69.999%': {
-            opacity: 0.4,
-            filter: 'none',
-          },
+        emerge: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
         },
-        shimmer: {
-          '0%': {
-            backgroundPosition: '-700px 0',
-          },
-          '100%': {
-            backgroundPosition: '700px 0',
-          },
+        fade: {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
+        emergeUp: {
+          from: { opacity: 0, transform: "translateY(4px)" },
+          to: { opacity: 1, transform: "translateY(0)" },
+        },
+        slideInBottom: {
+          from: { transform: "translateY(100%)" },
+          to: { transform: "translateY(0)" },
+        },
+        slideOutBottom: {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(100%)" },
+        },
+        slideInLeft: {
+          from: { transform: "translateX(-100%)" },
+          to: { transform: "translateX(0)" },
+        },
+        slideOutLeft: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+        slideInRight: {
+          from: { transform: "translateX(100%)" },
+          to: { transform: "translateX(0)" },
+        },
+        slideOutRight: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(100%)" },
+        },
+        scaleUp: {
+          from: {  opacity: 0, transform: "scale(0.6)" },
+          to: {  opacity: 1, transform: "scale(1)" },
+        },
+        scaleDown: {
+          from: { opacity: 1,  ransform: "scale(1)" },
+          to: {  opacity: 0, transform: "scale(0.6)" },
+        },
+        scaleInLeft: {
+          from: { opacity: 0, transform: "scaleX(0)", transformOrigin: "center left" },
+          to: { opacity: 1, transform: "scaleX(1)", transformOrigin: "center left" },
+        },
+        scaleOutLeft: {
+          from: { opacity: 1, transform: "scaleX(1)", transformOrigin: "center left" },
+          to: { opacity: 0, transform: "scaleX(0)", transformOrigin: "center left" },
+        },
+        scaleInRight: {
+          from: { opacity: 0, transform: "scaleX(0)", transformOrigin: "center right" },
+          to: { opacity: 1, transform: "scaleX(1)", transformOrigin: "center right" },
+        },
+        scaleOutRight: {
+          from: { opacity: 1, transform: "scaleX(1)", transformOrigin: "center right" },
+          to: { opacity: 0, transform: "scaleX(0)", transformOrigin: "center right" },
         },
       },
+      zIndex: {
+        100: "100",
+        200: "200",
+        300: "300",
+        400: "400",
+        500: "500",
+      },
       animation: {
-        flicker: 'flicker 3s linear infinite',
-        shimmer: 'shimmer 1.3s linear infinite',
+        emerge: "emerge 0.1s ease-in",
+        fade: "fade .1s ease-out forwards",
+        "emerge-up": "emergeUp .1s ease-in",
+        "slide-in-bottom": "slideInBottom .1s ease-in",
+        "slide-out-bottom": "slideOutBottom .1s ease-out forwards",
+        "scale-up": "scaleUp .1s ease-in",
+        "scale-down": "scaleDown .1s ease-out forwards",
+        "slide-in-left": "slideInLeft .1s ease-in",
+        "slide-out-left": "slideOutLeft .1s ease-out forwards",
+        "slide-in-right": "slideInRight .1s ease-in",
+        "slide-out-right": "slideOutRight .1s ease-out forwards",
+        "scale-in-left": "scaleInLeft .1s ease-in",
+        "scale-out-left": "scaleOutLeft .1s ease-out forwards",
+        "scale-in-right": "scaleInRight .1s ease-in",
+        "scale-out-right": "scaleOutRight .1s ease-out forwards",
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
-};
+}
