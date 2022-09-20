c7e8cc5c006208556e6662521db10deae7a09d74
HoangPham <hoangpm@greengroups.vn>
2022-09-20 11:52:12 +0700

    feat: responsive news and blog page; thêm breadcrumbs

--- src/components/shared/chip/chip.tsx
+++ src/components/shared/chip/chip.tsx
@@ -3,10 +3,10 @@ interface ChipProps {
   text: string;
 }
 
-export function Chip({ className = '', text = '', ...props }: ChipProps) {
+export function Chip({ className = '', text = '' }: ChipProps) {
   return (
     <div
-      className={`rounded bg-[#E7F7F0] px-4 py-1 text-xs text-primary shadow-sm ${className}`}
+      className={`rounded bg-[#E7F7F0] px-4 py-1 text-xs text-primary shadow-sm lg:text-base ${className}`}
     >
       {text}
     </div>
