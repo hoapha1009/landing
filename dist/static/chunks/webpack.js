ad864c27f5f61ec4468f5304e3e0a9680712be5c
HieuTran <trantrunghieu777888555@gmail.com>
2022-09-21 22:49:27 +0700

    fix: background

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -198,7 +198,7 @@
   /******/ /* webpack/runtime/getFullHash */
   /******/ !(function () {
     /******/ __webpack_require__.h = function () {
-      return 'e3eb051a8e96772c';
+      return 'f03dfca1524afd98';
     };
     /******/
   })();
