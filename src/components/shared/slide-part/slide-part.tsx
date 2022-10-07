b52b2ddecdb0c6989d141e49ccf4d8444ad4a9c0
HoangPham <hoangpm@greengroups.vn>
2022-10-07 14:01:47 +0700

    feat: đường dẫn các trang chủ, trang giới thiệu qua form ở trang green check

--- src/components/shared/slide-part/slide-part.tsx
+++ src/components/shared/slide-part/slide-part.tsx
@@ -13,7 +13,11 @@ export function SlidePart({
   type = 'partner',
 }: SlidePartProps) {
   return (
-    <div className={`py-8 lg:py-24 ${className}`} data-aos='fade-up'>
+    <div
+      className={`py-8 lg:py-24 ${className}`}
+      data-aos='fade-up'
+      data-aos-delay={600}
+    >
       <div className='main-container'>
         <Title text={title} />
         <div className='mt-8 rounded-md border border-gray-200 bg-white p-12 shadow-md'>
