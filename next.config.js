b2b4bf1e7d63ef7377bbefea7e4018626250fa5c
HoangPham <hoangpm@greengroups.vn>
2022-10-18 14:19:46 +0700

    feat: install and use bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,33 +1,75 @@
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  eslint: {
-    dirs: ['src'],
-  },
-  reactStrictMode: false,
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  // your other plugins here
+  {
+    eslint: {
+      dirs: ['src'],
+    },
+    reactStrictMode: false,
 
-  // Uncoment to add domain whitelist
-  // images: {
-  //   domains: [
-  //     'res.cloudinary.com',
-  //   ],
-  // },
+    // Uncoment to add domain whitelist
+    // images: {
+    //   domains: [
+    //     'res.cloudinary.com',
+    //   ],
+    // },
 
-  // SVGR
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            typescript: true,
-            icon: true,
+    // SVGR
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              icon: true,
+            },
           },
-        },
-      ],
-    });
+        ],
+      });
 
-    return config;
+      return config;
+    },
   },
-};
+]);
+
+// module.exports = {
+//   eslint: {
+//     dirs: ['src'],
+//   },
+//   reactStrictMode: false,
+
+//   // Uncoment to add domain whitelist
+//   // images: {
+//   //   domains: [
+//   //     'res.cloudinary.com',
+//   //   ],
+//   // },
+
+//   // SVGR
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/i,
+//       issuer: /\.[jt]sx?$/,
+//       use: [
+//         {
+//           loader: '@svgr/webpack',
+//           options: {
+//             typescript: true,
+//             icon: true,
+//           },
+//         },
+//       ],
+//     });
+
+//     return config;
+//   },
+// };
