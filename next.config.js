12cbaa2846dd232bb39bb7ca3093296bdfdcac84
HoangPham <hoangpm@greengroups.vn>
2022-10-18 14:27:11 +0700

    feat: use bundle ananlyzer ver 4

--- next.config.js
+++ next.config.js
@@ -1,45 +1,40 @@
-const withPlugins = require('next-compose-plugins');
+/** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
 });
-
-/** @type {import('next').NextConfig} */
-module.exports = withPlugins([
-  [withBundleAnalyzer],
-  // your other plugins here
-  {
-    eslint: {
-      dirs: ['src'],
-    },
-    reactStrictMode: false,
-
-    // Uncoment to add domain whitelist
-    // images: {
-    //   domains: [
-    //     'res.cloudinary.com',
-    //   ],
-    // },
-
-    // SVGR
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              typescript: true,
-              icon: true,
-            },
-          },
-        ],
-      });
-
-      return config;
-    },
+module.exports = withBundleAnalyzer({
+  eslint: {
+    dirs: ['src'],
   },
-]);
+  reactStrictMode: false,
+
+  // Uncoment to add domain whitelist
+  // images: {
+  //   domains: [
+  //     'res.cloudinary.com',
+  //   ],
+  // },
+
+  // SVGR
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            typescript: true,
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+});
 
 // module.exports = {
 //   eslint: {
