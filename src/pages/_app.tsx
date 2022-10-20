1f69cf182e67e751aeeecab6f526dc3e685eaae7
HoangPham <hoangpm@greengroups.vn>
2022-10-20 16:40:56 +0700

    feat: seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,31 +14,31 @@ import { DefaultSeo } from 'next-seo';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo
+        titleTemplate='%s | Green Agri'
+        defaultTitle='Green Agri'
+        description='Công ty Cổ phần Công nghệ Green Agri là đơn vị tiên phong trong lĩnh vực cung cấp các giải pháp công nghệ cho ngành nông nghiệp'
+        canonical='https://greenagri.com.vn/'
+        openGraph={{
+          type: 'website',
+          url: 'https://greenagri.com.vn/',
+          title: 'Green Agri',
+          description:
+            'Công ty Cổ phần Công nghệ Green Agri là đơn vị tiên phong trong lĩnh vực cung cấp các giải pháp công nghệ cho ngành nông nghiệp',
+          images: [
+            {
+              url: 'https://i.imgur.com/TEWDd5D.png',
+              width: 1200,
+              height: 630,
+              alt: 'Công ty Cổ phần Công nghệ Green Agri là đơn vị tiên phong trong lĩnh vực cung cấp các giải pháp công nghệ cho ngành nông nghiệp',
+            },
+          ],
+          site_name: 'Green Agri',
+        }}
+      />
       <DefaultHead />
       <ScreenProvider>
         <Layout>
-          <DefaultSeo
-            titleTemplate='%s | Green Agri'
-            defaultTitle='Green Agri'
-            description='Công ty Cổ phần Công nghệ Green Agri là đơn vị tiên phong trong lĩnh vực cung cấp các giải pháp công nghệ cho ngành nông nghiệp'
-            canonical='https://greenagri.com.vn/'
-            openGraph={{
-              type: 'website',
-              url: 'https://greenagri.com.vn/',
-              title: 'Green Agri',
-              description:
-                'Công ty Cổ phần Công nghệ Green Agri là đơn vị tiên phong trong lĩnh vực cung cấp các giải pháp công nghệ cho ngành nông nghiệp',
-              images: [
-                {
-                  url: 'https://i.imgur.com/TEWDd5D.png',
-                  width: 1200,
-                  height: 630,
-                  alt: 'Công ty Cổ phần Công nghệ Green Agri là đơn vị tiên phong trong lĩnh vực cung cấp các giải pháp công nghệ cho ngành nông nghiệp',
-                },
-              ],
-              site_name: 'Green Agri',
-            }}
-          />
           <Component {...pageProps} />
         </Layout>
       </ScreenProvider>
